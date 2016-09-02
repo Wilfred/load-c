2df275afba02bd4723dc1b9102bd72e9c21efb07
Wilfred Hughes <me@wilfred.me.uk>
2016-09-01 21:52:34 -0400

    Skeleton file

---
+++ download-c.el
@@ -0,0 +1,31 @@
+;;; download-c.el --- download C sources for the current Emacs  -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2016  
+
+;; Author: Wilfred Hughes <me@wilfred.me.uk>
+;; Keywords: c
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; Emacs does not ship with C source code. This is package is to help
+;; you fix that.
+
+;;; Code:
+
+
+
+(provide 'download-c)
+;;; download-c.el ends here
